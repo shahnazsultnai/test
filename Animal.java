800974583d2fcc95f8d55ec1f7b5f5adbdd155a2
Shahnaz Sultani <115884220+shahnazsultnai@users.noreply.github.com>
2022-12-16 09:32:17 +0330

    firstcommits

---
+++ Animal.java
@@ -0,0 +1,21 @@
+package Animal;
+
+public abstract class Animal {
+
+    String name;
+    String geneder;
+    int weight;
+    String color;
+
+    abstract void eat();
+    abstract void drink();
+    abstract void run();
+
+
+
+
+
+
+
+
+}
---
+++ Cat.java
@@ -0,0 +1,8 @@
+package Animal;
+
+public  class Cat extends Animal  {
+
+
+
+
+}
